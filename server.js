e76f609c2d2d9a7e0e8e4cbbfc6c77b4f1a6dd87
Muhammad Waqar Khan <63258252+waqarkhan09099@users.noreply.github.com>
2021-10-29 10:26:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path=require("path");
 
 
 app.use('/',(req,res,next)=>{
-    console.log("came Request form server",new Date())
+    console.log("came Request form server",Date.now())
     next()
 })
 
 app.get('/',(req,res)=>{
-    res.send("Hello World")
+    res.send("Hello World !!! Muhammad Waqar Khan Ai And Chatbot")
 })
 app.get('/name',(req,res)=>{
     res.send("My name is Khan")
